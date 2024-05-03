62a710f9d9862bb300a0331c679e158a8375b13b
jdeastin <j.d.east@icloud.com>
2024-05-03 15:02:01 -0700

    added stylish load spinner for feedback list

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       // make request to api for data
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
@@ -17,13 +20,15 @@ export default function FeedbackList() {
       })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <ol className="feedback-list">
+      {isLoading ? <Spinner /> : null}
       {feedbackItems.map((feedbackItem) => (
-        <FeedbackItem key={feedbackItem['id']} feedbackItem={feedbackItem} />
+        <FeedbackItem key={feedbackItem["id"]} feedbackItem={feedbackItem} />
       ))}
     </ol>
   );
