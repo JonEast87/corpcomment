cc391655f23d8cb4a52767e416c8d2187d7c2b06
jdeastin <j.d.east@icloud.com>
2024-05-03 15:12:08 -0700

    added basic error messaging if issue with loading feedback occurs

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -2,24 +2,35 @@ import { useEffect, useState } from "react";
 
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
+
     fetch(
       // make request to api for data
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
+      // receeived the data in json format
       .then((response) => {
-        // receeived the data in json format
+        if (!response.ok) {
+          throw new Error();
+        }
         return response.json();
-        // converting that data to javascript to be used in realtime
       })
       .then((data) => {
+        // converting that data to javascript to be used in realtime
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        // network error, not 2xx response, or JSON parsing error
+        setErrorMessage("Something went wrong");
         setIsLoading(false);
       });
   }, []);
@@ -27,6 +38,9 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem["id"]} feedbackItem={feedbackItem} />
       ))}
